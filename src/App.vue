11ab02071df2c9fa124b8abfe2a142647e314d0b
Robert Gleason <l2ob122@gmail.com>
2023-06-30 17:36:47 -0700

    finished

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import TodoApp from './components/TodoApp.vue';
+</script>
+
+<template>
+  <todo-app />
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
